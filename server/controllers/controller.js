6c52edbac06f09b50419292908cd68f0071ccc1c
JIG ERA <jig111era@gmail.com>
2025-09-19 22:51:54 +0530

    ..

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,18 +1,18 @@
-import { TODO_API } from '../constants/constant.js'
+// import { TODO_API } from '../constants/constant.js'
 import axios from 'axios'
 
 export const getTodos = async (req, res) => {
     try {
-        const { data } = await axios.get(TODO_API);
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/todos/`);
         return res.status(200).json({
             success: true,
-            message: `Data fetched successfully from ${TODO_API}`,
+            message: `Data fetched successfully from ${`https://jsonplaceholder.typicode.com/todos/`}`,
             data: data
         })
     } catch (err) {
         return res.status(500).json({
             success: false,
-            message: `Failed to fetch data from ${TODO_API}`,
+            message: `Failed to fetch data from ${`https://jsonplaceholder.typicode.com/todos/`}`,
             error: err.message
         })
     }
